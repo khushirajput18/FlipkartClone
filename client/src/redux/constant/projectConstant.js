c191f3c16e5167809ae09546ffcd5c084db14ffa
khushirajput18 <156101909+khushirajput18@users.noreply.github.com>
2025-06-08 18:10:00 +0530

    Initial commit

--- client/src/redux/constant/projectConstant.js
+++
@@ -1,7 +0,0 @@
-export const GET_PRODUCTS_SUCCESS='getProductSuccess';
-export const  GET_PRODUCTS_FAIL='getProductFail';
-
-export const GET_PRODUCT_DETAILES_REQUEST="getProductDetailesRequest";
-export const GET_PRODUCT_DETAILES_SUCCESS="getProductDetailesSucess";
-export const GET_PRODUCT_DETAILES_FAIL="getProductDetailesFail";
-export const GET_PRODUCT_DETAILES_RESET="getProductDetailesReset";
